037838cb913a5d8eb2fd2e3be958f305f8f01ebe
Søren Bruus Frank <soerenbf@gmail.com>
2016-11-25 11:00:57 +0100

    Mapped routes to menu from server rendered JSON.

--- ClientApp/app/app.component.tsx
+++ ClientApp/app/app.component.tsx
@@ -2,14 +2,21 @@ import * as React from "react";
 import { Link } from 'react-router';
 
 interface IAppComponentProps {};
-interface IAppComponentState {};
+interface IAppComponentState {
+    menuItems: any[];
+};
 
 export class AppComponent extends React.Component<IAppComponentProps, IAppComponentState> {
+    componentWillMount() {
+        this.setState(Object.assign({}, this.state, {menuItems: window['menuItems']}));
+    }
+
     render(): JSX.Element {
         return (
             <span>
-                <Link to="/">Products</Link>
-                <Link to="/test">Test</Link>
+                {this.state.menuItems.map((menuItem, key) => (
+                    <Link to={menuItem.Url}>{menuItem.Title}</Link>
+                ))}
                 {this.props.children}
             </span>
         );

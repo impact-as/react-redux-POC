1ce638a3e23a901e93bcdda06e3d5f157317e2f3
Søren Bruus Frank <soerenbf@gmail.com>
2016-12-30 15:30:36 +0100

    Font-awesome added.

--- ClientApp/product-filter/product-filter.component.tsx
+++ ClientApp/product-filter/product-filter.component.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
+import { resolveFilter } from './product-filter.service';
+
 class StatelessProductFilter extends React.Component<any, any> {
     render(): JSX.Element {
         return (
-            <div className="product-filter">Product filter...</div>
+            <div className="product-filter">
+                <button className="product-filter__button product-filter__button--heart">
+                    <i className="fa fa-heart" aria-hidden="true"></i>
+                </button>
+            </div>
         );
     }
 }

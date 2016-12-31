95ae6b8e41506f209f02e7be71dba236c64e9655
Søren Bruus Frank <soerenbf@gmail.com>
2016-12-31 12:01:12 +0100

    Added right click handler for remove from basket

--- ClientApp/product/add-to-basket.component.tsx
+++ ClientApp/product/add-to-basket.component.tsx
@@ -3,13 +3,24 @@ import * as React from 'react';
 interface IAddToBasketProps {
     addToBasket: () => void;
     removeFromBasket: () => void;
+    count: number;
 };
 
 export class AddToBasket extends React.Component<IAddToBasketProps, void> {
+    handleRightClick = (e: React.MouseEvent<HTMLButtonElement>): boolean => {
+        e.preventDefault();
+        this.props.removeFromBasket();
+        
+        return false;
+    }
+
     public render(): JSX.Element {
         return (
             <div className="add-to-basket">
-                <button onClick={this.props.addToBasket}>BUY!</button>
+                <button onClick={this.props.addToBasket}
+                        onContextMenu={this.handleRightClick}>
+                    {(this.props.count ? this.props.count : <i className="fa fa-shopping-cart" aria-hidden="true"></i>)}
+                </button>
             </div>
         );
     }

fcb7086416d3f194c6e454f48a10cdcc1053c109
Søren Bruus Frank <soerenbf@gmail.com>
2017-01-07 22:49:37 +0100

    Changed file extension to tsx.

--- webpack-config/webpack.server.js
+++ webpack-config/webpack.server.js
@@ -1,6 +1,6 @@
 // Configuration for server-side (prerendering) bundle suitable for running in Node
 module.exports = {
-    entry: { 'server-render': './ClientApp/server-render.ts' },
+    entry: { 'server-render': './ClientApp/server-render.tsx' },
     resolve: { extensions: [ '', '.js', '.jsx', '.ts', '.tsx' ] },
     output: {
         libraryTarget: 'commonjs',
